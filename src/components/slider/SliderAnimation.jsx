f4f4686abc645ba6def8568dba8011441e168fcf
themaryanjuguna <themaryanjuguna@gmail.com>
2024-02-16 03:06:10 +0300

    image update

--- src/components/slider/SliderAnimation.jsx
+++ src/components/slider/SliderAnimation.jsx
@@ -121,8 +121,7 @@ const Slider = () => {
         <div
           className="hb-me"
           style={{
-            backgroundImage: `url(${
-              process.env.PUBLIC_URL+"./img/slider/home-banner.png"
+            backgroundImage: `url(${"./img/slider/home-banner.png"
             })`,
           }}
         ></div>
