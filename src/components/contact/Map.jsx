9fcadb99a862300dae9942160801ecdceda4f4e4
themaryanjuguna <themaryanjuguna@gmail.com>
2024-12-06 21:15:19 +0300

    Map iframe update

--- src/components/contact/Map.jsx
+++ src/components/contact/Map.jsx
@@ -5,10 +5,16 @@ const Map = () => {
     <>
       <div className="google-map">
         <div className="embed-responsive embed-responsive-21by9">
-          <iframe
+        <iframe
             className="embed-responsive-item"
             title="location title"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3435462.690694435!2d37.840304943208345!3d-0.39539824887147995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f11f38ecb7a45%3A0x86a02ed6b9e73190!2sCreativeMania!5e0!3m2!1sen!2ske!4v1705323193070!5m2!1sen!2ske"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d53760175.46943987!2d-75.54652759999999!3d34.673369!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f110b36fe16ad%3A0x6a922a015d4f66c!2s%23usingiziinitiative!5e0!3m2!1sen!2ske!4v1708710316681!5m2!1sen!2ske"
+            width="600"
+            height="450"
+            style={{ border: "0" }} // Fix: Convert style attribute value to an object
+            allowFullScreen=""
+            loading="lazy"
+            referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
       </div>
