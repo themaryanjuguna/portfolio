dfc28f91a83f8b1e93e4127500e669ec05d9cd2d
themaryanjuguna <themaryanjuguna@gmail.com>
2024-12-11 01:10:45 +0300

    axios update

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -15,15 +15,12 @@ const Contact = () => {
   const onSubmit = async (formData) => {
     try {
       // POST request to backend
-      const response = await axios.post(
-        "https://porfoliobackend-3ja5.onrender.com/submit",
-        formData,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      const API_URL = process.env.REACT_APP_API_URL;
+      const response = await axios.post(API_URL, formData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
       console.log("Form submitted successfully:", response.data);
       alert("Your message has been sent successfully!");
@@ -31,7 +28,10 @@ const Contact = () => {
       // Reset form fields
       reset();
     } catch (error) {
-      console.error("Error submitting the form:", error.response?.data || error.message);
+      console.error(
+        "Error submitting the form:",
+        error.response?.data || error.message
+      );
       alert("There was an issue submitting your message. Please try again.");
     }
   };
@@ -86,7 +86,9 @@ const Contact = () => {
                 {...register("subject", { required: "Subject is required" })}
               />
               {errors.subject && (
-                <span className="invalid-feedback">{errors.subject.message}</span>
+                <span className="invalid-feedback">
+                  {errors.subject.message}
+                </span>
               )}
             </div>
           </div>
@@ -101,7 +103,9 @@ const Contact = () => {
                 {...register("comment", { required: "Comment is required" })}
               ></textarea>
               {errors.comment && (
-                <span className="invalid-feedback">{errors.comment.message}</span>
+                <span className="invalid-feedback">
+                  {errors.comment.message}
+                </span>
               )}
             </div>
           </div>
